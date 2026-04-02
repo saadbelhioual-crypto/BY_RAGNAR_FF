572fcdb0679860de05f75dcecb2de8e4d6a39540
saadbelhioual-crypto <saadbelhioual@gmail.com>
2026-04-02 19:32:49 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ragnar-neon-blue': '#00f2ff',
+        'ragnar-neon-red': '#ff003c',
+      },
+      animation: {
+        'conic-rotate': 'rotate 4s linear infinite',
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
